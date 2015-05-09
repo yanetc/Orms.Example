16ed97a28e3a5b15fe47eb6d39877ca601d4cb09
Yanet <yanet.camejo@gmail.com>
2015-05-09 23:29:56 +0200

    Test DatabaseInfoRepository with Managed and unmanaged oracle client

---
+++ Oracle.Example.UnitTests/Repositories/DatabaseInfoRepositoryTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Configuration;
+using Dapper.Oracle.Example.Infrastructure;
+
+namespace Dapper.Oracle.Example.Repositories.Tests
+{
+    [TestClass()]
+    public class DatabaseInfoRepositoryTests
+    {
+        protected string connectionString = ConfigurationManager.ConnectionStrings["Oracle11g"].ConnectionString;
+
+
+
+        [TestMethod()]
+        public void GetDBStatusWithManagedClientTest()
+        {
+            using (var database = new DatabaseManaged(connectionString))
+            {
+                var dbInfoRepository = new DatabaseInfoRepository(database);
+                var dbStatus = dbInfoRepository.GetDBStatus();
+                Assert.IsNotNull(dbStatus);
+            }
+        }
+
+        [TestMethod()]
+        public void GetDBStatusWithUnManagedClientTest()
+        {
+            using (var database = new DatabaseUnManaged(connectionString))
+            {
+                var dbInfoRepository = new DatabaseInfoRepository(database);
+                var dbStatus = dbInfoRepository.GetDBStatus();
+                Assert.IsNotNull(dbStatus);
+            }
+        }
+
+
+    }
+}
